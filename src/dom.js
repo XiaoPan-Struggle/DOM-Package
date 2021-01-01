a4d87f14eba0c0b857e235a8b3033980d9ed1834
XiaoPan <2489964425@qq.com>
2021-01-01 16:45:08 +0800

    修改/查看 标签样式

--- src/dom.js
+++ src/dom.js
@@ -70,5 +70,22 @@ window.dom = {
     }else if(arguments.length === 1) {
       return node.innerHTML
     } 
+  },
+  style(node, name, value) {
+    // 字符串传值
+    if(arguments.length === 3) {
+      // dom.style(div,'color','red')
+      node.style[name] = value
+    }else if(arguments.length === 2) { // 对象传值
+      if(typeof name === 'string') {
+        // dom.style(div,'color')
+        return node.style[name]
+      }else if(name instanceof Object) {
+        // dom.style(div, {color: 'red'})
+        for(let key in name) {
+          node.style[key] = object[key]
+        }
+      }
+    } 
   }
 }
