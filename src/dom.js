2057b8fafe4ec665e6b3899d9779a6ae439df65f
XiaoPan <2489964425@qq.com>
2021-01-01 17:50:46 +0800

    遍历节点

--- src/dom.js
+++ src/dom.js
@@ -133,5 +133,10 @@ window.dom = {
       x = x.previousSibling
     }
     return x
+  },
+  each(nodeList, fn){
+    for(let i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i])
+    }
   }
 }
