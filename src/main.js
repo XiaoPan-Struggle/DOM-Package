240d269399e333545109322d28eece921be550b7
XiaoPan <2489964425@qq.com>
2021-01-01 18:09:08 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log(test.parentNode.insertBefore)
+console.log(dom)
+const div = dom.find('#test>.red')[0]
+console.log(div)
+
+dom.style(div,'color','red')
+
+const divList = dom.find('.red')
+dom.each(divList, (n) => {
+  console.log(n)
+})
